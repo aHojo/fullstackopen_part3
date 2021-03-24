5b9000cf7252a1e93286dc71df8cda1cc7606cd5
ahojo <ahojodev@gmail.com>
2021-03-24 15:20:10 -0400

    Added morgan to log post data send back to the client

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ let phonebook = [
 ]
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('content', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 app.get("/api/persons", (req,res) => {
 
 
